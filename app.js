ec787c93d35eb4f63598af384d7feb0e79546b5f
clayton <clayton@indeed.com>
2020-11-10 12:33:57 -0800

    efi

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.post('/upload', function(req, res) {
     return res.status(400).send('No files were uploaded.');
 
   }
-
+  res.send("file uploaded")
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   let sampleFile = req.files.sampleFile;
   let filename = req.files.sampleFile.name;
